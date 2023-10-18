7c129632a6ff395a634f5588b2abed28d6c763a7
haganenoubik <haganenoubik@gmail.com>
2023-10-18 22:38:12 +0900

    :test_tube: Test: Userモデルのテストを作成

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe User do
+  let(:name) { 'test' }
+  let(:email) { 'test@example.com' }
+
+  describe '.first' do
+    before do
+      create(:user, name: name, email: email)
+    end
+
+    subject { described_class.first }
+
+    it '事前に作成した通りのUserを返す' do
+      expect(subject.name).to eq('test')
+      expect(subject.email).to eq('test@example.com')
+    end
+  end
 end
