b256e9e67ae38c58fc616ea8979eb8b5c992cbd4
haganenoubik <haganenoubik@gmail.com>
2023-10-21 12:46:15 +0900

    :sparkles: Feat: deviseのビューファイルを生成

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,21 @@ module.exports = {
     './app/javascript/**/*.js',
   ],
   daisyui: {
-    themes: ["bumblebee"],
+    themes: ["bumblebee",
+      {
+        mytheme: {
+          "primary": "#00a3af",
+          "secondary": "#008899",
+          "accent": "#e17b34",
+          "neutral": "#18141f",
+          "base-100": "#FFFFFF",
+          "info": "#4d4398",
+          "success": "#00ac97",
+          "warning": "#fdd35c",
+          "error": "#ef857b",
+        },
+      },
+    ],
   },
   plugins: [require("daisyui")]
 }
