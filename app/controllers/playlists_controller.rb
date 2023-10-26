bce234829741aed6dd5dfef54709dfd4fa4aba1f
haganenoubik <haganenoubik@gmail.com>
2023-10-26 12:33:20 +0900

    :sparkles: Feat: Playlistsコントローラにアクションのベースを追加

--- app/controllers/playlists_controller.rb
+++ app/controllers/playlists_controller.rb
@@ -1,2 +1,24 @@
 class PlaylistsController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy 
+  end
 end
