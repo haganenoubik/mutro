18b508d320a60641d966ef0bfdc6233b52715ec1
haganenoubik <haganenoubik@gmail.com>
2023-12-17 15:56:14 +0900

    :sparkles: Feat: プロフィール編集時のストロングパラメータにnameカラムを追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,10 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
+    # 新規登録時のストロングパラメータ
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+
+    # プロフィール編集時のストロングパラメータ
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
   end
 end
