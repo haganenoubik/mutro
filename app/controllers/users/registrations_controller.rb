9fb0ac7b812bc93a733d8425042f1c5b1ee6edd3
haganenoubik <haganenoubik@gmail.com>
2023-12-19 13:59:27 +0900

    :rotating_light: Chore: Lintチェック指摘箇所を修正

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -38,15 +38,15 @@ class Users::RegistrationsController < Devise::RegistrationsController
   #   super
   # end
 
-    protected
+  protected
 
-    def update_resource(resource, params)
-      resource.update_without_password(params)
-    end
+  def update_resource(resource, params)
+    resource.update_without_password(params)
+  end
 
-    def after_update_path_for(resource)
-      user_path(resource)
-    end
+  def after_update_path_for(resource)
+    user_path(resource)
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_up_params
