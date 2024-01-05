94233e6cda2f82a90520555cd6898411c50f208c
haganenoubik <haganenoubik@gmail.com>
2024-01-06 07:43:51 +0900

    :adhesive_bandage: Chore: 検索結果を上位9つ表示するようにコードを修正

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -2,7 +2,8 @@ class TracksController < ApplicationController
 
   def search
     if params[:search].present?
-      @tracks = RSpotify::Track.search(params[:search])
+      search_results = RSpotify::Track.search(params[:search])
+      @tracks = search_results.first(9)
       respond_to do |format|
         format.turbo_stream
         format.html { render partial: "tracks/search_results", locals: { tracks: @tracks } }
