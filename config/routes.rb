6c6a10040fcd03a7f30f4cc6b503dbcd85c7f2c5
haganenoubik <haganenoubik@gmail.com>
2024-01-05 11:20:59 +0900

    :adhesive_bandage: Chore: ルーティング内のアクションの記述順序を修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'users/registrations' }
   root 'playlists#index'
 
-  resources :playlists, only: %i[show new create edit update destroy]
+  resources :playlists, only: %i[new create show edit update destroy]
   resources :users, only: %i[show]
 
   resources :tracks do
