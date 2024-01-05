44e3ab048b139a9d4b6ecb52915c65ea87de9160
haganenoubik <haganenoubik@gmail.com>
2024-01-05 11:56:48 +0900

    :sparkles: 曲検索機能をプレイリスト作成画面で表示できるよう非同期処理

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -1,8 +1,13 @@
 class TracksController < ApplicationController
 
   def search
-      if params[:search].present?
-        @tracks = RSpotify::Track.search(params[:search])
+    if params[:search].present?
+      @tracks = RSpotify::Track.search(params[:search])
+      respond_to do |format|
+        format.turbo_stream
+        format.html { render partial: "tracks/search_results", locals: { tracks: @tracks } }
       end
     end
   end
+end
+
