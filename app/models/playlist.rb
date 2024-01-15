4be8f2ccb67f69a155cbbc1e25b71b2f9269fbdd
haganenoubik <haganenoubik@gmail.com>
2024-01-15 23:22:55 +0900

    :sparkles: Feat: PlaylistとTrackの中間テーブルを用いた多対多のアソシエーションを追加及びPlaylistのステータスを管理するenumを追加

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -1,7 +1,13 @@
 class Playlist < ApplicationRecord
-  has_many :playlist_trackw
+  has_many :playlist_tracks
+  has_many :tracks, through: :playlist_tracks
   belongs_to :user
 
   validates :title, presence: true
   validates :description, presence: true
+
+  enum status: {
+    creating: 0,
+    published: 1  
+  } 
 end
