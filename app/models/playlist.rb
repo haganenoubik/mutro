3ca97bacbdc3c18729fb39f706e6ca730e0c7207
haganenoubik <haganenoubik@gmail.com>
2024-01-16 00:20:58 +0900

    :zap: Feat: titleの最大文字数を20文字に制限, descriptionの最小文字数を30文字, 最大文字数を1000文字に制限

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -3,8 +3,8 @@ class Playlist < ApplicationRecord
   has_many :tracks, through: :playlist_tracks
   belongs_to :user
 
-  validates :title, presence: true
-  validates :description, presence: true
+  validates :title, presence: true, uniqueness: true, length: { maximum: 20 }
+  validates :description, presence: true, length: { minimum: 30, maximum: 1000 }
 
   enum status: {
     creating: 0,
