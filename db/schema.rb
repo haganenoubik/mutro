36ff911b99146c2802d4cfff366318597bd535ee
haganenoubik <haganenoubik@gmail.com>
2024-01-15 23:31:04 +0900

    :sparkles: Feat: statusカラムとspotify_idカラムをマイグレーション実行により追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_12_22_075024) do
+ActiveRecord::Schema[7.0].define(version: 2024_01_08_065937) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -29,6 +29,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_12_22_075024) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "status"
     t.index ["user_id"], name: "index_playlists_on_user_id"
   end
 
@@ -37,6 +38,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_12_22_075024) do
     t.string "artist"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "spotify_id"
   end
 
   create_table "users", force: :cascade do |t|
