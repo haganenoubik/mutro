e3cff3be81f64c4a4b827c498e9ae64e4bcac2e2
haganenoubik <haganenoubik@gmail.com>
2024-01-16 16:42:45 +0900

    :sparkles: Feat: titleカラムのnull, uniquenessを許容するように一時変更

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -1,9 +1,9 @@
 class Playlist < ApplicationRecord
-  has_many :playlist_tracks
+  has_many :playlist_tracks, dependent: :destroy
   has_many :tracks, through: :playlist_tracks
   belongs_to :user
 
-  validates :title, presence: true, uniqueness: true, length: { maximum: 20 }
+  validates :title, length: { maximum: 20 }
   validates :description, presence: true, length: { minimum: 30, maximum: 1000 }
 
   enum status: {
