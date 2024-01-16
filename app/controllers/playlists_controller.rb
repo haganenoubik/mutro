c35b43f2ea76fb5a529e3f478359eddb8e215a76
haganenoubik <haganenoubik@gmail.com>
2024-01-16 16:47:22 +0900

    :sparkles: Feat: プレイリスト作成・詳細・一覧のコントローラを実装

--- app/controllers/playlists_controller.rb
+++ app/controllers/playlists_controller.rb
@@ -1,20 +1,31 @@
 class PlaylistsController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: %i[index show]
 
   def index
+    @playlists = Playlist.where(status: :published).order(created_at: :desc).includes(:user)
   end
 
   def show
+    @playlist = Playlist.find(params[:id])
   end
 
   def new
-    @playlist = current_user.playlists.find_or_initialize_by(status: "creating")
+    @playlist = current_user.playlists.find_by(status: :creating) || current_user.playlists.new
   end
 
   def create
-    @playlist = current_user.playlists.new(playlist_params) 
-    @playlist.status = :creating 
-    if @playlist.save
+    # セッションからプレイリストIDを取得
+    @playlist = current_user.playlists.find_by(id: session[:playlist_id])
+
+    # プレイリストが見つからない場合の処理
+    unless @playlist
+      @playlist = current_user.playlists.new(playlist_params)
+      @playlist.status = :creating
+    end
+
+    # プレイリストを更新
+    if @playlist.update(playlist_params.merge(status: :published))
+      session.delete(:playlist_id) # セッションからIDを削除
       redirect_to playlist_path(@playlist)
     else
       render :new
@@ -31,16 +42,16 @@ class PlaylistsController < ApplicationController
   end
 
   def add_track_to_playlist
-    @playlist = current_user.playlists.find_or_create_by(status: :creating)
-    spotify_track = RSpotify::Track.find(params[:track_id])
+    @playlist = current_user.playlists.find_by(status: :creating) || current_user.playlists.create(status: :creating)
+    session[:playlist_id] = @playlist.id
 
+    spotify_track = RSpotify::Track.find(params[:track_id])
     @track = Track.find_or_create_by(spotify_id: params[:track_id]) do |t|
       t.title = spotify_track.name
       t.artist = spotify_track.artists.first.name
     end
 
-    @playlist.tracks << @track
-    @playlist.tracks.reload
+    @playlist.tracks << @track unless @playlist.tracks.include?(@track)
 
     respond_to do |format|
       format.turbo_stream do
@@ -52,7 +63,7 @@ class PlaylistsController < ApplicationController
   private
 
   def playlist_params
-    params.require(:playlist).permit(:title, :description, track_ids: [])
+    params.require(:playlist).permit(:title, :description)
   end
 
 end
