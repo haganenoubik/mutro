02942cd3af3679cee96786d7d9ac67b6b4ea8301
haganenoubik <haganenoubik@gmail.com>
2024-01-17 15:54:08 +0900

    :sparkles: Feat: Playlistモデルのデフォルト値の設定とバリデーションの調整

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -1,13 +1,22 @@
 class Playlist < ApplicationRecord
+  before_validation :set_default_values, on: :create
+
   has_many :playlist_tracks, dependent: :destroy
   has_many :tracks, through: :playlist_tracks
   belongs_to :user
 
   validates :title, length: { maximum: 20 }
-  validates :description, presence: true, length: { minimum: 30, maximum: 1000 }
+  validates :description, length: { maximum: 1000 }
 
   enum status: {
     creating: 0,
     published: 1  
   } 
+
+  private
+
+  def set_default_values
+    self.title ||= ""
+    self.description ||= ""
+  end
 end
