4599a7efe45d1f42242f00c236293fd850d70d17
haganenoubik <haganenoubik@gmail.com>
2024-01-18 02:35:50 +0900

    :sparkles: Feat: Commnetsテーブルを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_01_17_145327) do
+ActiveRecord::Schema[7.0].define(version: 2024_01_17_163047) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "playlist_id", null: false
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["playlist_id"], name: "index_comments_on_playlist_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
 
   create_table "playlist_tracks", force: :cascade do |t|
     t.bigint "playlist_id", null: false
@@ -56,6 +66,8 @@ ActiveRecord::Schema[7.0].define(version: 2024_01_17_145327) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "comments", "playlists"
+  add_foreign_key "comments", "users"
   add_foreign_key "playlist_tracks", "playlists"
   add_foreign_key "playlist_tracks", "tracks"
   add_foreign_key "playlists", "users"
