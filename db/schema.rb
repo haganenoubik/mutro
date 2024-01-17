c7e3d7ac8c0ab89bf0b70e9c5bc38fc2b54a43fa
haganenoubik <haganenoubik@gmail.com>
2024-01-18 00:51:51 +0900

    :sparkles: Feat: Usersモデルにbioカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_01_17_071327) do
+ActiveRecord::Schema[7.0].define(version: 2024_01_17_145327) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -51,6 +51,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_01_17_071327) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "bio"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
