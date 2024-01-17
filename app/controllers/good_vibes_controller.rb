80f4a362697676d71365f5552a2e0ea3c7813c0b
haganenoubik <haganenoubik@gmail.com>
2024-01-18 04:31:03 +0900

    :sparkles: Feat: クリック時の非同期処理に対応できるように変更

--- app/controllers/good_vibes_controller.rb
+++ app/controllers/good_vibes_controller.rb
@@ -1,2 +1,30 @@
 class GoodVibesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_playlist
+
+  def create
+    unless current_user.good_vibed?(@playlist)
+      @playlist.good_vibes.create!(user: current_user)
+    end
+    respond_to do |format|
+      format.turbo_stream
+      format.html { redirect_to @playlist }
+    end
+  end
+
+  def destroy
+    good_vibe = @playlist.good_vibes.find_by(user_id: current_user.id)
+    good_vibe&.destroy
+    respond_to do |format|
+      format.turbo_stream
+      format.html { redirect_to @playlist }
+    end
+  end
+
+  private
+
+  def set_playlist
+    @playlist = Playlist.find(params[:playlist_id])
+  end
 end
+
