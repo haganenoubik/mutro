0d49e6d93a3c627268702328db9086fd2ea473a6
haganenoubik <haganenoubik@gmail.com>
2024-01-17 15:59:52 +0900

    :sparkles: Feat: セッションに現在のプレイリストを保存する機能を追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,30 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  def set_current_playlist(playlist)
+    session[:current_playlist_id] = playlist.id
+  end
+
+  private
+
+  def current_playlist
+    if session[:current_playlist_id]
+      @current_playlist ||= Playlist.find_by(id: session[:current_playlist_id])
+    else
+      @current_playlist = current_user.playlists.new(status: :creating)
+      if @current_playlist.save
+        session[:current_playlist_id] = @current_playlist.id
+      else
+        logger.debug "Failed to create new playlist: #{@current_playlist.errors.full_messages}"
+      end
+    end
+
+    logger.debug "Session current_playlist_id: #{session[:current_playlist_id]}"
+    logger.debug "Current playlist: #{@current_playlist.inspect}"
+    
+    @current_playlist
+  end
+
   protected
 
   def configure_permitted_parameters
