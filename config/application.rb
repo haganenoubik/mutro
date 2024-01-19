f1836d9567b4e6b76e4ddc60abf614e837d3e9b4
haganenoubik <haganenoubik@gmail.com>
2024-01-19 10:28:47 +0900

    :sparkles: Feat: TimezoneをTokyoに変更

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,7 @@ module Mutro
   class Application < Rails::Application
     config.load_defaults 7.0
     config.i18n.default_locale = :ja
+    config.time_zone = 'Tokyo'
 
     # Spotify APIの認証情報を設定
     RSpotify::authenticate(ENV['SPOTIFY_CLIENT_ID'], ENV['SPOTIFY_CLIENT_SECRET'])
