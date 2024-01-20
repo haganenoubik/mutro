a601d93043e7ae3eaaf14d8f919e61ff36abe769
haganenoubik <haganenoubik@gmail.com>
2024-01-20 20:24:57 +0900

    :sparkles: Feat: Playlistモデルのtitle, descriptionにpresence: trueを設定

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -1,6 +1,4 @@
 class Playlist < ApplicationRecord
-  before_validation :set_default_values, on: :create
-
   has_many :playlist_tracks, dependent: :destroy
   has_many :tracks, through: :playlist_tracks
   has_many :comments, dependent: :destroy
@@ -8,18 +6,11 @@ class Playlist < ApplicationRecord
   has_many :good_vibed_users, through: :good_vibes, source: :user
   belongs_to :user
 
-  validates :title, length: { maximum: 20 }
-  validates :description, length: { maximum: 1000 }
+  validates :title, presence: true, length: { maximum: 20 }
+  validates :description, presence: true, length: { maximum: 1000 }
 
   enum status: {
     creating: 0,
     published: 1
   }
-
-  private
-
-  def set_default_values
-    self.title ||= ''
-    self.description ||= ''
-  end
 end
