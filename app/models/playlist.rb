91c2318291e3db8a13fd52761ad8e88e897aab48
haganenoubik <haganenoubik@gmail.com>
2024-01-22 13:37:52 +0900

    :art: Chore: descriptionの入力文字数を10000に修正

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -7,7 +7,7 @@ class Playlist < ApplicationRecord
   belongs_to :user
 
   validates :title, presence: true, length: { maximum: 20 }
-  validates :description, presence: true, length: { maximum: 3000 }
+  validates :description, presence: true, length: { maximum: 10000 }
   validate :tracks_count_within_limit
 
   enum status: {
