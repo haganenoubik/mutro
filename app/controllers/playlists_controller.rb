e4983726e102e2a2a9a0f314dce50d4ed3367df6
haganenoubik <haganenoubik@gmail.com>
2024-01-23 16:12:08 +0900

    :sparkles: Feat: remove_track_to_playlistアクションを追加, 曲追加時のturboの指定先をplaylist_previewに変更

--- app/controllers/playlists_controller.rb
+++ app/controllers/playlists_controller.rb
@@ -70,9 +70,20 @@ class PlaylistsController < ApplicationController
 
     respond_to do |format|
       format.turbo_stream do
-        render turbo_stream: turbo_stream.append('playlist_tracks', partial: 'playlists/track', locals: { track: @track })
+        render turbo_stream: turbo_stream.append('playlist_preview', partial: 'playlists/track', locals: { track: @track })
       end
     end
+  end
+
+  def remove_track_from_playlist
+    session[:current_playlist_tracks].delete(params[:track_id].to_i)
+
+    respond_to do |format|
+      format.turbo_stream do
+        render turbo_stream: turbo_stream.remove("track_#{params[:track_id]}")
+      end
+    end
+
   end
 
   def new_releases
