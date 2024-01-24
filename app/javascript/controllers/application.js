dd1d4356b0bec3c2e1e60e4899a466c1f015cce3
haganenoubik <haganenoubik@gmail.com>
2024-01-25 00:25:38 +0900

    :sparkles: Feat: Application.jsの設定

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,8 +1,12 @@
+// stimulusのパッケージからApplicationをimportしている
 import { Application } from "@hotwired/stimulus"
+import AutoCompleteController from "./autocomplete_controller"
 
 const application = Application.start()
+// HTML内で、data-controller="autocomplete"と指定された要素を
+// AutoCompleteControllerで制御するように登録している
+application.register('autocomplete', AutoCompleteController)
 
-// Configure Stimulus development experience
 application.debug = false
 window.Stimulus   = application
 
