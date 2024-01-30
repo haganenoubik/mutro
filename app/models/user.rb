31367433fdd8642f5f40888f8bb83d1f91474ccd
haganenoubik <haganenoubik@gmail.com>
2024-01-30 23:31:49 +0900

    :sparkles: Feat: userモデルにself.from_omniauthアクションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,28 @@ class User < ApplicationRecord
   has_many :good_vibes, dependent: :destroy
   has_many :good_vibed_playlists, through: :good_vibes, source: :playlist
 
+  def self.from_omniauth(auth)
+    user = User.where(email: auth.info.email).first
+
+    if user
+      # ユーザーが既に存在する場合、プロバイダとUIDを更新
+      user.provider = auth.provider
+      user.uid = auth.uid
+      user.save
+    else
+      # ユーザーが存在しない場合、新しく作成
+      user = User.create(
+        name: auth.info.name,
+        email: auth.info.email,
+        password: Devise.friendly_token[0, 20],
+        provider: auth.provider,
+        uid: auth.uid
+      )
+    end
+
+    user
+  end
+
   # 特定のプレイリストに対してGood Vibesの有無を確認
   def good_vibed?(playlist)
     good_vibes.exists?(playlist_id: playlist.id)
