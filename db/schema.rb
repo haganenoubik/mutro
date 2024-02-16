9bd5282958bebd3c03ea6838a3d1255a82addc85
haganenoubik <haganenoubik@gmail.com>
2024-02-16 16:11:38 +0900

    :sparkles: Feat: Moodモデルと関連ファイルを生成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_02_13_113950) do
+ActiveRecord::Schema[7.0].define(version: 2024_02_16_045359) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -32,6 +32,13 @@ ActiveRecord::Schema[7.0].define(version: 2024_02_13_113950) do
     t.index ["playlist_id"], name: "index_good_vibes_on_playlist_id"
     t.index ["user_id", "playlist_id"], name: "index_good_vibes_on_user_id_and_playlist_id", unique: true
     t.index ["user_id"], name: "index_good_vibes_on_user_id"
+  end
+
+  create_table "moods", force: :cascade do |t|
+    t.string "name"
+    t.string "image_filename"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "playlist_tracks", force: :cascade do |t|
---
+++ spec/factories/moods.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :mood do
+    name { "MyString" }
+  end
+end
