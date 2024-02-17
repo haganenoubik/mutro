71d319ac08bd4ec260e4c97a2074331f8dd5e13b
haganenoubik <haganenoubik@gmail.com>
2024-02-17 19:47:14 +0900

    :art: Style: ラジオボタンの色を変更

--- app/helpers/playlists_helper.rb
+++ app/helpers/playlists_helper.rb
@@ -2,17 +2,17 @@ module PlaylistsHelper
   def mood_radio_color(mood_name)
     case mood_name
     when '選択しない'
-      'checked:bg-slate-800'
+      'checked:bg-slate-300'
     when '都会的'
-      'checked:bg-violet-800'
+      'checked:bg-violet-300'
     when '気分を上げる'
-      'checked:bg-green-500'
+      'checked:bg-red-300'
     when 'リラックス'
-      'checked:bg-blue-500'
+      'checked:bg-emerald-300'
     when '懐かしい'
-      'checked:bg-red-500'
+      'checked:bg-yellow-300'
     else
-      'checked:bg-gray-500'
+      'checked:bg-gray-300'
     end
   end
 end
