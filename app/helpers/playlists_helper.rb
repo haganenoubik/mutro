d09a98ffdec8c6f79f5f7978720f2b3e00e7181c
haganenoubik <haganenoubik@gmail.com>
2024-02-17 17:33:46 +0900

    :sparkles: Feat: Moodのラジオボタンのスタイリングを追加

--- app/helpers/playlists_helper.rb
+++ app/helpers/playlists_helper.rb
@@ -1,2 +1,18 @@
 module PlaylistsHelper
+  def mood_radio_color(mood_name)
+    case mood_name
+    when '選択しない'
+      'checked:bg-slate-800'
+    when '都会的'
+      'checked:bg-violet-800'
+    when '気分を上げる'
+      'checked:bg-green-500'
+    when 'リラックス'
+      'checked:bg-blue-500'
+    when '懐かしい'
+      'checked:bg-red-500'
+    else
+      'checked:bg-gray-500'
+    end
+  end
 end
