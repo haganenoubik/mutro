4583b1a5f87d17f36323bbbf86c5dddc8cde03f8
haganenoubik <haganenoubik@gmail.com>
2024-02-17 17:32:17 +0900

    :sparkles: Feat: Moodに"選択しない"を追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+moods = [
+  { name: '選択しない', image_filename: nil },
+  { name: '都会的', image_filename: 'urban-image.png' },
+  { name: '気分を上げる', image_filename: 'energy-image.png' },
+  { name: 'リラックス', image_filename: 'chill-image.png' },
+  { name: '懐かしい', image_filename: 'nostalgic-image.png' }
+]
+
+moods.each do |mood_data|
+  Mood.find_or_create_by(name: mood_data[:name]) do |mood|
+    mood.image_filename = mood_data[:image_filename]
+  end
+end
